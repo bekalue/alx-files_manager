4450d15b6701d8023c1ffb90606a47e96e74d988
Bekalu Geta <bekalu2054@gmail.com>
2023-09-03 17:43:11 +0300

    🔧Fix Error occurance

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -167,32 +167,36 @@ export default class FilesController {
   static async getIndex(req, res) {
     const { user } = req;
     const parentId = req.query.parentId || ROOT_FOLDER_ID.toString();
-    const page = /\d+/.test((req.query.page || '').toString()) ? Number.parseInt(req.query.page, 10) : 0;
+    const page = /\d+/.test((req.query.page || '').toString())
+      ? Number.parseInt(req.query.page, 10)
+      : 0;
     const filesFilter = {
       userId: user._id,
       parentId: parentId === ROOT_FOLDER_ID.toString()
-        ? parentId : new mongoDBCore.BSON.ObjectId(isValidId(parentId) ? parentId : NULL_ID),
+        ? parentId
+        : new mongoDBCore.BSON.ObjectId(isValidId(parentId) ? parentId : NULL_ID),
     };
 
-    const files = await (await (await dbClient.filesCollection()).aggregate([
-      { $match: filesFilter },
-      { $sort: { _id: -1 } },
-      { $skip: page * MAX_FILES_PER_PAGE },
-      { $limit: MAX_FILES_PER_PAGE },
-      {
-        $project: {
-          _id: 0,
-          id: '$_id',
-          userId: '$userId',
-          name: '$name',
-          type: '$type',
-          isPublic: '$isPublic',
-          parentId: {
-            $cond: { if: { $eq: ['$parentId', '0'] }, then: 0, else: '$parentId' },
+    const files = await (await (await dbClient.filesCollection())
+      .aggregate([
+        { $match: filesFilter },
+        { $sort: { _id: -1 } },
+        { $skip: page * MAX_FILES_PER_PAGE },
+        { $limit: MAX_FILES_PER_PAGE },
+        {
+          $project: {
+            _id: 0,
+            id: '$_id',
+            userId: '$userId',
+            name: '$name',
+            type: '$type',
+            isPublic: '$isPublic',
+            parentId: {
+              $cond: { if: { $eq: ['$parentId', '0'] }, then: 0, else: '$parentId' },
+            },
           },
         },
-      },
-    ])).toArray();
+      ])).toArray();
     res.status(200).json(files);
   }
 }
