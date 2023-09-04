77bc942e5047c668418a5752381a88e74cd55485
Bekalu Geta <bekalu2054@gmail.com>
2023-09-04 11:30:17 +0300

    `feat`: ✅Complete `db` and `redis` test

---
+++ tests/utils/redis.test.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-named-as-default */
+import { before, describe, it } from 'mocha';
+import { expect } from 'chai';
+import redisClient from '../../utils/redis';
+
+describe('+ RedisClient utility', () => {
+  before(function (done) {
+    this.timeout(10000);
+    setTimeout(done, 4000);
+  });
+
+  it('+ Client is alive', () => {
+    expect(redisClient.isAlive()).to.equal(true);
+  });
+
+  it('+ Setting and getting a value', async () => {
+    await redisClient.set('test_key', 345, 10);
+    expect(await redisClient.get('test_key')).to.equal('345');
+  });
+
+  it('+ Setting and getting an expired value', async () => {
+    await redisClient.set('test_key', 356, 1);
+    setTimeout(async () => {
+      expect(await redisClient.get('test_key')).to.not.equal('356');
+    }, 2000);
+  });
+
+  it('+ Setting and getting a deleted value', async () => {
+    await redisClient.set('test_key', 345, 10);
+    await redisClient.del('test_key');
+    setTimeout(async () => {
+      console.log('del: test_key ->', await redisClient.get('test_key'));
+      expect(await redisClient.get('test_key')).to.be.null;
+    }, 2000);
+  });
+});
