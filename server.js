c384f37323d241e79622b801223cbb486e9d8e7c
Bekalu Geta <bekalu2054@gmail.com>
2023-09-05 14:39:00 +0300

    Complete task

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './routes/index';
+import envLoader from './utils/env_loader';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,7 +9,7 @@ const env = process.env.npm_lifecycle_event || 'dev';
 app.use(express.json({ limit: '200mb' }));
 app.use('/', routes);
 
-// envLoader();
+envLoader();
 app.listen(port, () => {
   console.log(`[${env}] => Server running on http://localhost:${port}`);
 });
